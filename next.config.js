f9268369524d615a74d4ca0c8b77f59debd7dbe9
vipinkumar71 <vipinthakur3881@gmail.com>
2025-03-17 19:04:36 +0530

    configuration changes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even with ESLint errors
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Warning: This allows production builds to successfully complete even with TypeScript errors
     ignoreBuildErrors: true,
   },
   images: {
